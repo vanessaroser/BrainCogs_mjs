e463e52db02524d2d40ae439266d251f4a4ffc69
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-10-03 17:27:42 -0400

    Create startup.m

---
+++ TaskLearning_VTA/startup.m
@@ -0,0 +1,8 @@
+addpath('/Toolboxes/mym/distribution/mexa64');
+
+addpath(genpath('../../../U19-pipeline-matlab'))
+setenv('DB_PREFIX', 'u19_')
+
+host = 'datajoint00.pni.princeton.edu';
+
+dj.conn(host)
