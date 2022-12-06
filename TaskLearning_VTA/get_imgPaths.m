5f542f215b85589d9e353ad00d456b872ade2912
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-12-06 12:02:51 -0500

    Update get_imgPaths.m

--- TaskLearning_VTA/get_imgPaths.m
+++ TaskLearning_VTA/get_imgPaths.m
@@ -13,14 +13,14 @@ C = calculate;
 F = figures;
 if any([C.combined_data, C.cellF, F.FOV_mean_projection])
     for i = 1:numel(expData)
+        %ROI Directory
         list = dir(fullfile(dirs.data,expData(i).sub_dir,'ROI*'));
         expData(i).roi_dir = list.name; %Full path to ROI directory
+        %Full Paths to Imaging Data
+        %  ***To Do: Make an independent function for this repo, or copy from iCorre.***
         [~, paths] = iCorreFilePaths( dirs.data, expData(i).sub_dir, []);
-        
         expData(i).raw_path = paths.raw;
-        
-        list = dir(fullfile(dirs.data,expData(i).sub_dir,'registered-chan1','*.tif'));
-        expData(i).reg_path = {list(:).name}';
+        expData(i).reg_path = paths.registered;
         
         %expData(i).mat_path = expData(idx).mat_path(:);
         %expData = get_imgPathnames(dirs,expData,i); %Get pathnames to raw, registered, and matfiles
